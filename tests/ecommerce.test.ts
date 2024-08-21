a14ae1faeeeb0f22639275c665b8d54058046b83
Amy <amy.gh.ly@gmail.com>
2024-08-21 13:51:59 +1000

    add test

--- tests/ecommerce.test.ts
+++ tests/ecommerce.test.ts
@@ -4,10 +4,17 @@ import {
   addToCart,
   calculateTotal,
   clearCart,
+  applyShippingDiscount
 } from "../src/ecommerce";
 
-let cart = {};
-describe("E-commerce System", () => {
+// describe - group related tests together
+// it - individual test case
+// expect - compare/ assertion of result
+// before each - allow for set up 
+
+
+
+describe("E-commerce System", () => { 
   beforeEach(() => {
     addProduct("Soap", 100, 10);
     addProduct("Shampoo", 200, 5);
@@ -15,9 +22,69 @@ describe("E-commerce System", () => {
   });
 
   it("should calculate price of all products", () => {
+
+    // arrange, act and assert - test pattern
+    // arrange
     addToCart("Soap", 2);
     addToCart("Shampoo", 2);
+
+    // act
     const sum = calculateTotal();
+
+    // assert
     expect(sum).toBe(600);
   });
+
+  it("should add items to cart", () => {
+
+    // arrange
+    let cart = {};
+
+    // act
+    cart = addToCart("Soap", 2);
+
+    // assert
+    expect(cart["Soap"]).toBe(2)
+  });
+
+  it("should add $10 shipping discount", ()=> {
+
+    // arrange
+    let cart = {};
+    addToCart("Soap", 1);
+    addToCart("Shampoo", 2);
+
+    // act
+    let sum = calculateTotal();
+
+    sum = applyShippingDiscount(sum);
+
+    // assert
+    expect(sum).toBe(490);
+  })
+
+  it("should not add $10 shipping discount", ()=> {
+
+    // arrange
+    let cart = {};
+    addToCart("Soap", 1);
+    addToCart("Shampoo", 1);
+
+    // act
+    let sum = calculateTotal();
+
+    sum = applyShippingDiscount(sum);
+
+    // assert
+    expect(sum).toBe(300);
+  });
+
+  it("should return insufficient stock", ()=>{
+
+    // arrange
+    let cart = {};
+
+    // assert
+    expect(() => addToCart("Beans",1)).toThrowError(/^Insufficient stock$/)
+  })
 });
